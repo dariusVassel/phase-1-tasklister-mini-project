ca947abb31e0301de266ddc18d7082a5c04dd437
+ <+>
2021-07-14 17:22:53 -0400

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+    form.addEventListener("submit", e => {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value)
+    form.reset()
+  })
+
+  // creating an element to add to the DOM 
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "x"
+  p.textContent = `${todo} `
+  //console.log(p)
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+//handling a delete feature
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
 });
